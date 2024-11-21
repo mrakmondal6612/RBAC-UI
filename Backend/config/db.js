3d4d84ad2f74eb77d210ac99817261abb0aea63d
Ajay Mondal <121559193+mrakmondal6612@users.noreply.github.com>
2024-11-21 09:27:26 +0000

    pending

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,9 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MongoURL, {
-      // Removed deprecated options
-    });
+    await mongoose.connect(process.env.MongoURL, {});
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(`Error: ${error.message}`);
